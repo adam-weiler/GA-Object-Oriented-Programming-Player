6e3205a66aa2f2965564b4683b5cc81b82ea22b4
adam-weiler <adam.weiler@yahoo.ca>
2019-07-15 15:44:00 -0400

    Adding do_battle method.

--- exercise3.py
+++ exercise3.py
@@ -16,22 +16,38 @@ class Player():
         if self.gold_coins % 10 == 0:
             self.level_up()
 
+    def do_battle(self, damage):
+        self.health_points -= damage
+
+        if self.health_points < 1:
+            self.lives -= 1
+            self.health_points = 10
+
+        if self.lives == 0:
+            pass
+
 
 
 player_one = Player()
-player_one.level_up()
-player_one.level_up()
-player_one.level_up()
-player_one.collect_treasure()
-player_one.collect_treasure()
-player_one.collect_treasure()
-player_one.collect_treasure()
-player_one.collect_treasure()
-player_one.collect_treasure()
-player_one.collect_treasure()
-player_one.collect_treasure()
-player_one.collect_treasure()
-player_one.collect_treasure()
+# player_one.level_up()
+# player_one.level_up()
+# player_one.level_up()
+# player_one.collect_treasure()
+# player_one.collect_treasure()
+# player_one.collect_treasure()
+# player_one.collect_treasure()
+# player_one.collect_treasure()
+# player_one.collect_treasure()
+# player_one.collect_treasure()
+# player_one.collect_treasure()
+# player_one.collect_treasure()
+# player_one.collect_treasure()
+
+player_one.do_battle(5)
+player_one.do_battle(6)
+player_one.do_battle(11)
+
+
 print(player_one)
 print()
 
