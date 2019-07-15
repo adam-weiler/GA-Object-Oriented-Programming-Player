5b720ebfe14616a6998cf59266099522d98d01b0
adam-weiler <adam.weiler@yahoo.ca>
2019-07-15 15:36:59 -0400

    Adding collect_treasure method.

--- exercise3.py
+++ exercise3.py
@@ -4,16 +4,38 @@ class Player():
         self.health_points = 10
         self.lives = 5
 
-    def __str__():
+    def __str__(self):
         return f'Player instance:gold_coins={self.gold_coins} health_points={self.health_points} lives={self.lives}'
 
+    def level_up(self):
+        self.lives += 1
+
+    def collect_treasure(self):
+        self.gold_coins += 1
+        
+        if self.gold_coins % 10 == 0:
+            self.level_up()
 
 
 
 player_one = Player()
+player_one.level_up()
+player_one.level_up()
+player_one.level_up()
+player_one.collect_treasure()
+player_one.collect_treasure()
+player_one.collect_treasure()
+player_one.collect_treasure()
+player_one.collect_treasure()
+player_one.collect_treasure()
+player_one.collect_treasure()
+player_one.collect_treasure()
+player_one.collect_treasure()
+player_one.collect_treasure()
 print(player_one)
-
+print()
 
 
 
 player_two = Player()
+print(player_two)
