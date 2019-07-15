4cfcf2ae15516c7f78ab1fee22e4f33237783201
adam-weiler <adam.weiler@yahoo.ca>
2019-07-15 15:31:19 -0400

    Creating exercise 3 file.

---
+++ exercise3.py
@@ -0,0 +1,19 @@
+class Player():
+    def __init__(self):
+        self.gold_coins = 0
+        self.health_points = 10
+        self.lives = 5
+
+    def __str__():
+        return f'Player instance:gold_coins={self.gold_coins} health_points={self.health_points} lives={self.lives}'
+
+
+
+
+player_one = Player()
+print(player_one)
+
+
+
+
+player_two = Player()
