afa3a3c9ba431de71218ca81972223a80bde39dc
adam-weiler <adam.weiler@yahoo.ca>
2019-07-20 12:05:57 -0400

    Rewrting str() method, adding some print statements to explain what's happening.

--- exercise3.py
+++ exercise3.py
@@ -1,12 +1,13 @@
 class Player(): #A Player class.
-    def __init__(self): #Every Player has attributes gold_coins, health_points, lives.
-        self.restart() #Calls the restart method to set values to default.
+    def __init__(self, name): #Every Player has attributes name, gold_coins, health_points, lives.
+        self.restart(name) #Calls the restart method to set values to default.
 
     def __str__(self): #Returns a meaningful string that describes the instance.
-        return f'Player instance:gold_coins={self.gold_coins} health_points={self.health_points} lives={self.lives}'
+        return f'{self.name} has {self.gold_coins} gold coins, {self.health_points} health points, and {self.lives} lives.'
 
     def level_up(self): #An instance method that adds 1 to the Player's lives.
         self.lives += 1
+        print('You gained a level!')
 
     def collect_treasure(self): #An instance method that adds 1 to the Player's gold_coins. 
         self.gold_coins += 1
@@ -20,17 +21,22 @@ class Player(): #A Player class.
         if self.health_points < 1: #If Player's health_points are less than 1, subtract a life and set health_points to 10.
             self.lives -= 1
             self.health_points = 10
+            print('You lost a life.')
 
         if self.lives == 0: #If Player's lives equals 0, Player's points are reset.
-            self.restart()
+            print('You lost all your lives.')
+            self.restart(self.name)
 
-    def restart(self): #Resets all values to their inital values.
+    def restart(self, name): #Resets all values to their inital values.
+        self.name = name
         self.gold_coins = 0
         self.health_points = 10
         self.lives = 5
+        print('New Game.')
 
 
-player_one = Player()
+player_one = Player('Mario')
+print(player_one) #Returns Mario has 0 gold coins, 10 health points, and 5 lives.
 player_one.collect_treasure()
 player_one.collect_treasure()
 player_one.collect_treasure()
@@ -41,16 +47,17 @@ player_one.collect_treasure()
 player_one.collect_treasure()
 player_one.collect_treasure()
 player_one.collect_treasure() #Player levels up and gains a life.
-print(player_one) #Returns Player instance:gold_coins=10 health_points=10 lives=6
+print(player_one) #Returns Mario has 10 gold coins, 10 health points, and 6 lives.
 print()
 
-player_two = Player()
+player_two = Player('Luigi')
+print(player_two) #Returns Luigi has 0 gold coins, 10 health points, and 5 lives.
 player_two.do_battle(10)
 player_two.do_battle(10)
 player_two.do_battle(10)
 player_two.do_battle(10)
 player_two.do_battle(10) #Player dies and is reset.
-print(player_two) #Returns Player instance:gold_coins=0 health_points=10 lives=5
+print(player_two) #Returns Luigi has 0 gold coins, 10 health points, and 5 lives.
 
 #These are extra things inside the player_two object.
 # print (player_two.__dict__) #Dictionary containing the class's namespace: {'gold_coins': 0, 'health_points': 10, 'lives': 5}
